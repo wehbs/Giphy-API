9a8518c9384b16ced4616a231e839fe59228273a
Shamir Wehbe <shamirwehbe@Shamirs-MacBook-Pro.local>
2017-09-10 23:46:10 -0400

    first commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+
+
+
+
+var topics = [];
+
+
+
+
+
+
+
+
+
+
+// Example queryURL for Giphy API
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+
+$.ajax({
+  url: queryURL,
+  method: 'GET'
+}).done(function(response) {
+  console.log(response.data[0].images.original_still.url);
+  $("#pic").attr("src", response.data[0].images.original_still.url);
+});
+
+
+
+
+
+
+
+
+});
