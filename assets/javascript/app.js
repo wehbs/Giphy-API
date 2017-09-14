efa51d12d9177825276dd779dc6125d9cc5efec7
Shamir Wehbe <shamirwehbe@Shamirs-MacBook-Pro.local>
2017-09-14 00:58:10 -0400

    adding buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 
 
-var topics = [];
+var foods = ["pizza", "hotdog", "eggplant", "salad", "potatoes"];
 
 
 
@@ -12,7 +12,7 @@ var topics = [];
 
 
 
-
+console.log(foods);
 
 
 // Example queryURL for Giphy API
@@ -26,8 +26,37 @@ $.ajax({
   $("#pic").attr("src", response.data[0].images.original_still.url);
 });
 
+function renderButtons() {
+
+  // Deleting the movies prior to adding new movies
+  // (this is necessary otherwise you will have repeat buttons)
+  $("#foodButtons").empty();
+
+  for (var i = 0; i < foods.length; i++) {
+
+    var a = $("<button>");
+    a.addClass("food");
+    a.attr("data-name", foods[i]);
+    a.text(foods[i]);
+    $("#foodButtons").append(a);
+  }
+}
+
+// This function handles events where a movie button is clicked
+$("#addFood").on("click", function(event) {
+  event.preventDefault();
+  // This line grabs the input from the textbox
+  var food = $("#foodInput").val().trim();
+
+  // Adding movie from the textbox to our array
+  foods.push(food);
+
+  // Calling renderButtons which handles the processing of our movie array
+  renderButtons();
+});
 
 
+renderButtons()
 
 
 
