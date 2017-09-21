c7f248e19b40e510a181da8a53538489f874a498
wehbs <shamirwehbe@me.com>
2017-09-20 20:10:18 -0400

    added some more commenting

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,33 +7,35 @@ $(document).ready(function () {
 
   // Add new button
   $("#addFood").on("click", function (event) {
+    // Prevents the page from reloading as this is the default action for a submit button in a form
     event.preventDefault();
-
+    // Gets the value of the text box input and also removes spaces before and after the text
     var food = $("#foodInput").val().trim();
-
+    // Add the new search term to the foods array
     foods.push(food);
 
     renderButtons();
-
+    // Clear out the text field after adding a new search button
     $("#foodInput").val("")
-    
+
   });
 
   // Search the Giphy Api based on the value of the button clicked
   function searchGiphyAPI() {
-
+    // Clears out the results from the previous search before populating new results
     $("#foods").empty();
+    // Captures the value of the data-name attribute from the button that was pressed
     var foodSearch = $(this).attr("data-name");
 
     // QueryURL for Giphy API
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + foodSearch + "&api_key=dc6zaTOxFJmzC";
-
+    // Ajax call to pull in the objects from the Giphy API
     $.ajax({
       url: queryURL,
       method: 'GET'
     }).done(function (response) {
       var results = response.data;
-
+      // Loops through only 10 gifs
       for (var i = 0; i < 10; i++) {
         // Create 10 Divs to hold gifs
         var foodDiv = $("<div class=col-md-4>" + "<br>");
